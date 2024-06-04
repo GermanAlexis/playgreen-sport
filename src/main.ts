d33d04026803593b4cbe841cb12829986dbe212f
German Alexis <alexisdager93@hotmail.com>
2024-06-04 02:23:58 -0500

    feature: configuration envoirements varibles

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { envs } from './config/envs';
+import { Logger, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
+  const logger = new Logger('Main');
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  await app.listen(envs.ports);
+
+  logger.log(`App running in port ${envs.ports}`);
 }
 bootstrap();
