672cd691543866f3bd2e9e96499ff5cc7b385f9f
German Alexis <alexisdager93@hotmail.com>
2024-06-04 09:16:38 -0500

    feature: conection mysql and added docker compose

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -3,11 +3,21 @@ import * as joi from 'joi';
 
 interface Envs {
   PORT: number;
+  HOST_DATABASE: string;
+  PORT_DATABASE: number;
+  USER_DATABASE: string;
+  PASS_DATABASE: string;
+  NAME_DATABASE: string;
 }
 
 const envsSchema = joi
   .object({
     PORT: joi.number().required(),
+    HOST_DATABASE: joi.string().required(),
+    PORT_DATABASE: joi.number().required(),
+    USER_DATABASE: joi.string().required(),
+    PASS_DATABASE: joi.string().required(),
+    NAME_DATABASE: joi.string().required(),
   })
   .unknown(true);
 
@@ -21,4 +31,9 @@ const envVars: Envs = value;
 
 export const envs = {
   ports: envVars.PORT,
+  host: envVars.HOST_DATABASE,
+  portDatabase: envVars.PORT_DATABASE,
+  userDataBase: envVars.USER_DATABASE,
+  passDataBase: envVars.PASS_DATABASE,
+  nameDatabase: envVars.NAME_DATABASE,
 };
